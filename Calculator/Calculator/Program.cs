daacc086510a1de737d918bf3688af8b10c4e6bb
H Duns <hannah.dunsmore@hotmail.com>
2025-04-10 11:56:24 +0100

    Add a simple multiplier

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -5,7 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to C# Calculator!");
-        string userInput = Console.ReadLine();
+        Console.WriteLine("Enter a number: ");
+        string firstNumber = Console.ReadLine();
+        Console.WriteLine("Enter a second number: ");
+        string secondNumber = Console.ReadLine();
+        Console.WriteLine($"{firstNumber} x {secondNumber} = {int.Parse(firstNumber) * int.Parse(secondNumber)}");
 
     }
 }
