746e482a9e75245984d700ff6c54058a8b86d43e
H Duns <hannah.dunsmore@hotmail.com>
2025-04-13 16:27:32 +0100

    Added date calculator

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -7,12 +7,106 @@ class Program
         PrintWelcomeMessage();
         bool wantCalculation;
         do
-        {        
-            NumberCalculator.PerformOneCalculation();
-            Console.WriteLine($"The answer to your calculation is: {NumberCalculator.CalculationAnswer}");
+        {
+            string calculator = ChooseCalculator();
+            Console.WriteLine($"You have chosen the {calculator} calculator!\n");
+            if (calculator == "Number")
+            {
+                NumberCalculator.PerformOneCalculation();
+                Console.WriteLine($"The answer to your calculation is: {NumberCalculator.CalculationAnswer}");
+            } else if (calculator == "Date")
+            {
+                Console.WriteLine("Please enter a date in the format DD/MM/YYYY: ");
+                DateTime userDateTime = DateTime.Parse(Console.ReadLine());
+                string dateCalculationOption = ChooseDateCalculationOption();
+                Console.WriteLine($"You have chosen to {dateCalculationOption}.");
+                // User inputs date information for calculation
+                Console.WriteLine($"Please enter the days you would like to {dateCalculationOption}. You can enter 0 if you wish.");
+                int userDays = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Please enter the months you would like to {dateCalculationOption}:");
+                int userMonths = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Please enter the years you would like to {dateCalculationOption}:");
+                int userYears = int.Parse(Console.ReadLine());
+                // Instance of DateCalculator intantitized
+                DateCalculator dC1 = new DateCalculator(userDateTime, userDays, userMonths, userYears);
+                if (dateCalculationOption == "Add")
+                    dC1.Add();
+                else
+                    dC1.Subtract();
+                Console.WriteLine($"The answer to your calculation is: {dC1.NewDate}");
+            }
             wantCalculation = RequestCalculcation();
         } while (wantCalculation);
-        
+    }
+
+    private static string ChooseCalculator()
+    {
+        bool choosenCalculator = false;
+        string calculatorChoice = "";
+
+        while (choosenCalculator == false)
+        {
+            Console.WriteLine("Which calculator do you want? \n 1) Numbers \n 2) Dates \nChoose Number");
+            string userChoice = Console.ReadLine();
+
+            if (int.TryParse(userChoice, out int choice))
+            {
+                if (choice == 1)
+                {
+                    calculatorChoice = "Number";
+                    choosenCalculator = true;
+                }
+                else if (choice == 2)
+                {
+                    calculatorChoice = "Date";
+                    choosenCalculator = true;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry we don't have that option.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{userChoice} is not a valid number. ");
+            }
+        }
+        return calculatorChoice;
+    }
+
+    private static string ChooseDateCalculationOption()
+    {
+        bool dateCalculationOptionChoosen = false;
+        string dateCalculationChoice = "";
+
+        while (!dateCalculationOptionChoosen)
+        {
+            Console.WriteLine("Would you like to add or subtract from your date? Choose option: \n1) Add \n2) Subtract");
+            string userInput = Console.ReadLine();
+            if (int.TryParse(userInput, out int choice))
+            {
+                if (choice == 1)
+                {
+                    dateCalculationChoice = "Add";
+                    dateCalculationOptionChoosen = true;
+                } 
+                else if (choice == 2)
+                {
+                    dateCalculationChoice = "Subtract";
+                    dateCalculationOptionChoosen = true;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry we don't have that option");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine($"{userInput} is not a valid option.");
+            }
+        }
+        return dateCalculationChoice;
     }
 
     private static void PrintWelcomeMessage()
@@ -20,8 +114,17 @@ class Program
         Console.WriteLine("Welcome to C# Calculator!");
         Console.WriteLine("=========================");
     }
+
+    public static bool RequestCalculcation()
+    {
+        Console.WriteLine("Would you like to calculate something else? (Yes or No)");
+        string userAnswer = Console.ReadLine().ToLower();
+        return userAnswer == "yes" ? true : false;
+    }
     
-    public class NumberCalculator()
+}
+
+public class NumberCalculator()
     {
         private static string OperatorSymbol
         {
@@ -143,11 +246,59 @@ class Program
         }
     }
 
-    public static bool RequestCalculcation()
+public class DateCalculator
+{
+    private DateTime OriginalDate
     {
-        Console.WriteLine("Would you like to calculate something else? (Yes or No)");
-        string userAnswer = Console.ReadLine().ToLower();
-        return userAnswer == "yes" ? true : false;
+        get;
+        set;
     }
     
+    public DateTime NewDate
+    {
+        get;
+        private set;
+    }
+
+    private int UserDays
+    {
+        get;
+        set;
+    }
+    
+    private int UserMonths
+    {
+        get;
+        set;
+    }
+    
+    private int UserYears
+    {
+        get;
+        set;
+    }
+
+    public DateCalculator(DateTime originalDate, int usersDays, int userMonths, int userYears)
+    {
+        OriginalDate = originalDate;
+        NewDate = originalDate;
+        UserDays = usersDays;
+        UserMonths = userMonths;
+        UserYears = userYears;
+    }
+
+    public void Add()
+    {
+        NewDate = NewDate.AddDays(UserDays);
+        NewDate = NewDate.AddMonths(UserMonths);
+        NewDate = NewDate.AddYears(UserYears);
+    }
+    
+    public void Subtract()
+    {
+        NewDate = NewDate.AddDays(-UserDays);
+        NewDate = NewDate.AddMonths(-UserMonths);
+        NewDate= NewDate.AddYears(-UserYears);
+    }
+
 }
--- Calculator/Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
+++ Calculator/Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+520460d3d8697a04b145a3d33cdd20d4ff56b6fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca840417dd9ca09eeb34f69ae774d8d790b34c8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
